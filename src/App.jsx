c528c8e29833d808cd8fa6a96b6271ad416d3961
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-19 18:03:11 +0100

    Create Homepage, Pricing, Product pages and define routes for them with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+
 function App() {
 
 
   return (
-    <>
-      Worldwise
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Homepage/>} />
+            <Route path="product" element={<Product/>} />
+            <Route path="pricing" element={<Pricing/>} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
