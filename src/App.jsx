5b08cb59de0db1d77508c5ff35967f8375786b8d
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-25 12:44:44 +0100

    Implement CountryList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useEffect, useState} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
@@ -6,7 +7,7 @@ import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList/CityList.jsx";
-import {useEffect, useState} from "react";
+import CountryList from "./components/CountryList/CountryList.jsx";
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -40,7 +41,7 @@ function App() {
             <Route path="app" element={<AppLayout/>}>
                 <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
                 <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-                <Route path="countries" element={<p>Countries</p>} />
+                <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
                 <Route path="form" element={<p>Form</p>} />
             </Route>
             <Route path="*" element={<PageNotFound />} />
